caee975df0d29f9e4e1e14f21efda6329172fd4d
PRAGATI <pragati2004muz@gmail.com>
2022-07-03 22:12:48 +0530

    main

--- App.js
+++ App.js
@@ -2,6 +2,12 @@ var express = require('express');
 var port = process.env.PORT || 8382
 var app = express();
 
+// firsy default route
+app.get('/',(req,res)=> {
+    res.send("Hii from Express")
+})
+
 app.listen(port,()=> {
     console.log('listening on port $(port)')
+
 })
